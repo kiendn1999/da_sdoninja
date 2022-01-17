18abc661558327147690cc71ff52d0b22ceeea9a
kiendn1999 <nguyenngockiendn1999@gmail.com>
2022-01-17 14:30:07 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:da_sdoninja/app/data/hive/hive_helper.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
